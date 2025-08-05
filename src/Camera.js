8ca47b4d9c4334d66f595d147ebbc14fb003dd7f
jhay <79243795+jjhay-bot@users.noreply.github.com>
2025-08-05 18:05:38 +0800

    Add files via upload

---
+++ src/Camera.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+import jsQR from "jsqr";
+
+const videoConstraints = {
+  width: { ideal: 1280 },
+  height: { ideal: 720 },
+  facingMode: "environment",
+};
+
+const Camera = () => {
+  const webcamRef = useRef(null);
+  const [url, setUrl] = React.useState(null);
+
+  const capturePhoto = React.useCallback(async () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setUrl(imageSrc);
+  }, [webcamRef]);
+
+  const onUserMedia = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <>
+      <Webcam
+        ref={webcamRef}
+        audio={true}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        onUserMedia={onUserMedia}
+      />
+      <button onClick={capturePhoto}>Capture</button>
+      <button onClick={() => setUrl(null)}>Refresh</button>
+      {url && (
+        <div>
+          <img src={url} alt="Screenshot" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Camera;
